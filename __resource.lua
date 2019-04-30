2f08cf8b7d32e462923706207d46cfed69d4799c
Tyler <48775431+Thymester@users.noreply.github.com>
2019-04-30 17:21:22 -0500

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+client_scripts {
+    "@NativeUI/NativeUI.lua",
+    "SimpleMenu.lua",
+}
+
+server_scripts {
+    "s_menu.lua"
+}
+
+client_scripts "SimpleMenu.lua"
+server_scripts "s_menu.lua"
