7d042447fdec7ba07e9ed336065655c4890ca1bd
Tyler <48775431+Thymester@users.noreply.github.com>
2019-05-03 01:24:10 -0500

    Delete s_menu.lua

--- s_menu.lua
+++
@@ -1,8 +0,0 @@
-RegisterServerEvent("admin:getIsAllowed")
-AddEventHandler("admin:getIsAllowed", function()
-    if IsPlayerAceAllowed(source, "simplemenu.admin") then
-        TriggerClientEvent("admin:returnIsAllowed", source, true)
-    else
-        TriggerClientEvent("admin:returnIsAllowed", source, false)
-    end
-end)
