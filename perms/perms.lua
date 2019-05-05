3f5e863d130932b3ff2df92dc1152a5c98f5ed51
Tyler <48775431+Thymester@users.noreply.github.com>
2019-05-05 05:58:49 -0500

    Add files via upload

--- perms/perms.lua
+++ perms/perms.lua
@@ -7,15 +7,6 @@ AddEventHandler("admin:getIsAllowed", function()
     end
 end)
 
-RegisterServerEvent("emergency:getIsemergency")
-AddEventHandler("emergency:getIsemergency", function()
-    if IsPlayerAceAllowed(source, "simplemenu.emergency") then
-        TriggerClientEvent("emergency:returnIsemergency", source, true)
-    else
-        TriggerClientEvent("emergency:returnIsemergency", source, false)
-    end
-end)
-
 RegisterServerEvent("fire:getIsFire")
 AddEventHandler("fire:getIsFire", function()
     if IsPlayerAceAllowed(source, "simplemenu.fire") then
@@ -34,6 +25,24 @@ AddEventHandler("emt:getIsEmt", function()
     end
 end)
 
+RegisterServerEvent("police:getIsPolice")
+AddEventHandler("police:getIsPolice", function()
+    if IsPlayerAceAllowed(source, "simplemenu.police") then
+        TriggerClientEvent("police:returnIsPolice", source, true)
+    else
+        TriggerClientEvent("police:returnIsPolice", source, false)
+    end
+end)
+
+RegisterServerEvent("sheriff:getIsSheriff")
+AddEventHandler("sheriff:getIsSheriff", function()
+    if IsPlayerAceAllowed(source, "simplemenu.sheriff") then
+        TriggerClientEvent("sheriff:returnIsSheriff", source, true)
+    else
+        TriggerClientEvent("sheriff:returnIsSheriff", source, false)
+    end
+end)
+
 RegisterServerEvent("state:getIsState")
 AddEventHandler("state:getIsState", function()
     if IsPlayerAceAllowed(source, "simplemenu.state") then
@@ -42,3 +51,21 @@ AddEventHandler("state:getIsState", function()
         TriggerClientEvent("state:returnIsState", source, false)
     end
 end)
+
+function SendWebhookMessage(webhook,message)
+    if webhook ~= "false" then
+        PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+    end
+end
+
+function mysplit(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
